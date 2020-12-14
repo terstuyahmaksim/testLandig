d5d0f02d8294c3088f7ba810521dbfc180ad9053
Maksim Terstuyah <terstuyah.maksim@yojji.io>
2020-12-14 17:08:59 +0200

    footer styles desktop

--- components/common/footer/footer.jsx
+++ components/common/footer/footer.jsx
@@ -4,99 +4,92 @@ export default function Footer() {
   return (
     <div className={styles.container}>
       <footer id="footer" className={styles.footer}>
-        <div className={styles.footerContainer}>
-          <div className={styles.footerFlex}>
-            <nav className={styles.footerNav}>
-              <div className={styles.gridColumn}>
-                <h4 className={styles.footerMenuHeader}>About Us</h4>
-                <ul className={styles.footerList}>
-                  <li>
-                    <a href="/pages/about">Who we are</a>
-                  </li>
-                  <li>
-                    <a href="/pages/reviews">Customer reviews</a>
-                  </li>
-                  <li>
-                    <a href="/pages/press">Press</a>
-                  </li>
-                  <li>
-                    <a href="/pages/team">Meet the team</a>
-                  </li>
-                  <li>
-                    <a href="/pages/careers">Careers</a>
-                  </li>
-                </ul>
-              </div>
-
-              <div className={styles.gridColumn}>
-                <h4 className={styles.footerMenuHeader}>Help & support</h4>
-                <ul className={styles.footerList}>
-                  <li>
-                    <a
-                      href="https://mrswordsmith.zendesk.com/hc/en-gb/categories/360001631291-Deliveries"
-                      rel="noopener"
-                    >
-                      Delivery info
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className={styles.nowrap}
-                      href="https://mrswordsmith.zendesk.com/hc/en-gb/categories/360001633212-Returns-Refunds"
-                    >
-                      Returns &amp; Refunds
-                    </a>
-                  </li>
-                  <li>
-                    <a className={styles.nowrap} href="/pages/privacy">
-                      Privacy Policy
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className={styles.nowrap}
-                      href="/pages/terms-and-conditions"
-                    >
-                      Terms &amp; Conditions
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className={styles.nowrap}
-                      href="https://mrswordsmith.zendesk.com/hc/en-us/categories/360002280817-Printables-FAQS"
-                    >
-                      Printables FAQs
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className={styles.nowrap}
-                      href="https://mrswordsmith.zendesk.com/hc/en-us/categories/360001633852-Products"
-                    >
-                      Books and Games FAQs
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className={styles.nowrap}
-                      href="https://mrswordsmith.zendesk.com/hc/en-us/categories/360002288797-Google-Classroom"
-                    >
-                      Google Classroom FAQs
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className={styles.nowrap}
-                      href="https://mrswordsmith.zendesk.com/hc/en-us/"
-                    >
-                      Other
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </nav>
+        <nav className={styles.footerNav}>
+          <div className={styles.gridColumn}>
+            <h4 className={styles.footerMenuHeader}>About Us</h4>
+            <ul className={styles.footerList}>
+              <li>
+                <a href="/pages/about">Who we are</a>
+              </li>
+              <li>
+                <a href="/pages/reviews">Customer reviews</a>
+              </li>
+              <li>
+                <a href="/pages/press">Press</a>
+              </li>
+              <li>
+                <a href="/pages/team">Meet the team</a>
+              </li>
+              <li>
+                <a href="/pages/careers">Careers</a>
+              </li>
+            </ul>
           </div>
-        </div>
+
+          <div className={styles.gridColumn}>
+            <h4 className={styles.footerMenuHeader}>Help & support</h4>
+            <ul className={styles.footerList}>
+              <li>
+                <a
+                  href="https://mrswordsmith.zendesk.com/hc/en-gb/categories/360001631291-Deliveries"
+                  rel="noopener"
+                >
+                  Delivery info
+                </a>
+              </li>
+              <li>
+                <a
+                  className={styles.nowrap}
+                  href="https://mrswordsmith.zendesk.com/hc/en-gb/categories/360001633212-Returns-Refunds"
+                >
+                  Returns &amp; Refunds
+                </a>
+              </li>
+              <li>
+                <a className={styles.nowrap} href="/pages/privacy">
+                  Privacy Policy
+                </a>
+              </li>
+              <li>
+                <a className={styles.nowrap} href="/pages/terms-and-conditions">
+                  Terms &amp; Conditions
+                </a>
+              </li>
+              <li>
+                <a
+                  className={styles.nowrap}
+                  href="https://mrswordsmith.zendesk.com/hc/en-us/categories/360002280817-Printables-FAQS"
+                >
+                  Printables FAQs
+                </a>
+              </li>
+              <li>
+                <a
+                  className={styles.nowrap}
+                  href="https://mrswordsmith.zendesk.com/hc/en-us/categories/360001633852-Products"
+                >
+                  Books and Games FAQs
+                </a>
+              </li>
+              <li>
+                <a
+                  className={styles.nowrap}
+                  href="https://mrswordsmith.zendesk.com/hc/en-us/categories/360002288797-Google-Classroom"
+                >
+                  Google Classroom FAQs
+                </a>
+              </li>
+              <li>
+                <a
+                  className={styles.nowrap}
+                  href="https://mrswordsmith.zendesk.com/hc/en-us/"
+                >
+                  Other
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </footer>
     </div>
   );
